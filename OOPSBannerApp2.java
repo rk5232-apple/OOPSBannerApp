4bd45550a548095913ec7716dc4de1a9f602d554
rk5232-apple <rk5232@srmist.edu.in>
2026-02-22 02:44:11 +0530

    Coded UC2 to Print OOPS to Console

---
+++ OOPSBannerApp2.java
@@ -0,0 +1,14 @@
+public class OOPSBannerApp2 {
+    public static void main(){
+        System.out.println("****  ****  ****  *****");
+        System.out.println("*  *  *  *  *  *  ** ");
+        System.out.println("*  *  *  *  *  *  ** ");
+        System.out.println("*  *  *  *  ****  *****");
+        System.out.println("*  *  *  *  *        **");
+        System.out.println("*  *  *  *  *        **");
+        System.out.println("****  ****  *     *****");
+		
+		return 0;
+
+    }
+}
